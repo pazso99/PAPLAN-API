b5e5197b11ddd87f621ad74da43ce8dca9c687c6
pazso99 <pzsolti99@gmail.com>
2023-11-15 20:45:27 +0100

    #3 sanctum init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\Auth\LoginResource;
+use App\Http\Resources\Auth\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('name', $request->name)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => ['Incorrect credentials'],
+            ]);
+        }
+
+        $user->tokens()->delete();
+
+        return new LoginResource($user);
+    }
+
+    public function getUser(Request $request)
+    {
+        return new UserResource($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+    }
+}
